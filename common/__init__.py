3169baf7f597f0bf5c2443aca4743a1845e105d8
xyl <xyl825923@163.com>
2023-06-15 21:31:41 +0800

    框架目录提交

---
+++ common/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project: Auto
+@Author : yuling xiao
+@Date : 2023/6/13 14:11
+@File : __init__.py.py
+@Describe:公共模块，存放一些通用方法
+"""
---
+++ testCase/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project: Auto
+@Author : yuling xiao
+@Date : 2023/6/13 14:12
+@File : __init__.py.py
+@Describe:测试用例代码
+"""
